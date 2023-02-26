eb8ca1e1f8af55aff7f768e404d703338301a559
zhugezuo <zhugezuo@users.noreply.github.com>
2023-02-26 16:48:34 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export default {
   // 填入你的OPENAI_API_KEY
-  OPENAI_API_KEY: '',
+  OPENAI_API_KEY: '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',
   // 设置获取消息的重试次数
   retryTimes: 3,
   // 在群组中设置唤醒微信机器人的关键词
